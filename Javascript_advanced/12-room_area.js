c80fa7e2ad2ea1956608433ff7f5dff7fae69a5d
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-01-28 20:10:42 +0100

    write code snippet to illustrate concept of binding

---
+++ Javascript_advanced/12-room_area.js
@@ -0,0 +1,10 @@
+let roomDimensions = {
+	width: 50,
+	length: 100,
+	getArea() {
+		return this.width * this.length;
+	}
+}
+
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+boundGetArea();
